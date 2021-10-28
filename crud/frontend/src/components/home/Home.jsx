e12d6ada897b0544f748b16b38fa7a8e4ae62046
kayo12 <kayonbahenrique@gmail.com>
2021-10-28 18:08:05 -0300

    Update

---
+++ crud/frontend/src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Inicio" subtitle="Segundo Projeto do Capitulo de React.">
+        <div className="display-4">Bem vindo !</div>
+        <hr />
+        <p className="mb-0">Sistema para exemplificar um template em react</p>
+    </Main>
