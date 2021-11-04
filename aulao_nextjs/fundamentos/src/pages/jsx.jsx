a33d1e2c590fd21e6c9014b60f994625edc82091
kayo12 <kayonbahenrique@gmail.com>
2021-11-04 00:22:45 -0300

    Update

---
+++ aulao_nextjs/fundamentos/src/pages/jsx.jsx
@@ -0,0 +1,11 @@
+export default function Jsx(){
+    const a = 4;
+    const b = 3
+    
+    return (
+        <div>
+            <h1>JSX é um conceito central</h1>
+            {a * b}
+        </div>
+    )
+}
