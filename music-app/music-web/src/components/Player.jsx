ea206f94e2b9b4a23ecc960d79e40fc8e60e7407
kayo12 <kayonbahenrique@gmail.com>
2021-11-13 19:01:41 -0300

    Update

--- music-app/music-web/src/components/Player.jsx
+++ music-app/music-web/src/components/Player.jsx
@@ -4,14 +4,24 @@ import imgPlayer from '../assests/img/music-song.png'
 export default props => 
     <div className="player">
        <div className="control-music">
-                <img src={imgPlayer} alt="img-songs"/> 
-           <div className="setup-music">
-                <button><i class="fa fa-backward"></i></button>
-                <button><i class="fa fa-play"></i></button>
-                <button><i class="fa fa-forward"></i></button>
+                <img src={imgPlayer} alt="img-songs"/>
+                
+            <div class="settings-music">    
+               <span>Orochi - Balão</span>
+               <div className="progress-bar">
+                   <div></div>
+               </div>
+               <div className="setup-music">
+                    <button><i class="fa fa-undo"></i></button>
+                    <button><i class="fa fa-backward"></i></button>
+                    <button><i class="fa fa-play"></i></button>
+                    <button><i class="fa fa-forward"></i></button>
+                    <button><i class="fa fa-volume-up"></i></button>     
+                </div>
            </div>
        </div>
        <div className="list-music">
+            
             <ul>
                 <li>1Lista de musicas</li>
                 <li>2Lista de musicas</li>
