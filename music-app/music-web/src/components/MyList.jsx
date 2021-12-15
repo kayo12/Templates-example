193a465c80380b5c69594e6187326a5e6d5e4f9b
kayo12 <35537031+kayo12@users.noreply.github.com>
2021-12-15 01:04:44 -0300

    Update

--- music-app/music-web/src/components/MyList.jsx
+++ music-app/music-web/src/components/MyList.jsx
@@ -10,7 +10,7 @@ const initial = {
 } 
 
 const getAuth = async () => {
-    const BASE64_ENCODED_AUTH_CODE = Buffer.from("c0fe3aacf5e3425cbbca4000d51f9255:60bf9c88879641048338831a166227ca").toString('base64');
+    const BASE64_ENCODED_AUTH_CODE = Buffer.from("c0fe3aacf5e3425cbbca4000d51f9255:60bf9c88879641048338831a166227ca", 'utf-8').toString('base64');
     const headers = {
       headers: {
         Accept: 'application/json',
@@ -27,7 +27,7 @@ const getAuth = async () => {
         qs.stringify(data),
         headers
       );
-      console.log(response.data.access_token);
+      console.log("token: ", response.data.access_token);
       return response.data.access_token;
     } catch (error) {
       console.log(error);
@@ -35,29 +35,38 @@ const getAuth = async () => {
   };
  
 
-  var list =  axios.get(
-                'https://api.spotify.com/v1/artists/0TnOYISbd1XYRBk9myaseg', {
-                    params: { limit: 50, offset: 0 },
-                    headers: {
-                        Accept: 'application/json',
-                        Authorization: 'Bearer ' + getAuth,
-                        'Content-Type': 'application/json',
-          },
-
+  const getAudioFeatures_Track = async (track_id) => {
+    //request token using getAuth() function
+    const access_token = await getAuth();
+    //console.log(access_token);
+  
+    const api_url = `https://api.spotify.com/v1/artists/${track_id}`;
+    //console.log(api_url);
+    try{
+      const response = await axios.get(api_url, {
+        headers: {
+          'Authorization': `Bearer ${access_token}`
+        }
       });
+      //console.log(response.data);
+      const dados = response.data
+      return dados
+    }catch(error){
+      console.log(error);
+    }  
+  };
     
-
 export default class Mylist extends Component {
 
     state = {...initial}
     render(){
-        console.log('lista de valores', list)
+      var obj = getAudioFeatures_Track('0TnOYISbd1XYRBk9myaseg');
+      console.log('DADOS: ', obj)
+      console.log("token: ", getAuth());
         return(
                 <div className="teste">
                      <span>{this.state.title}</span>
                      <p>{this.state.msg}</p>
-                    
-                     
                 </div>
         )
     }
