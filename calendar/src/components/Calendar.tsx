6d15819d473a644af830aec6a90af217e1fa7f3c
Kayo campos <kayonbahenrique@gmail.com>
2022-07-05 09:22:56 -0300

    Update

--- calendar/src/components/Calendar.tsx
+++ calendar/src/components/Calendar.tsx
@@ -3,14 +3,25 @@ import { useState, useEffect, Children } from 'react';
 import useCalendar from '../hooks/useCalendar'
 export default function Calendar(props) {
 
-    var cal = useCalendar()
+    const date = new Date( new Date().getFullYear(),1,0 );
+    var cal = useCalendar(date.getMonth())
+   
     // const rows = cal.arrDays.map((current) => {
     //    return <td>{current}</td>
     // })
+  function creteTable(){
+    cal.getMonth()
+      const list = cal.currentmonth.arrDays.map((element, index, arr) => {
+            for(let countX = 0; countX < arr[index].length; countX++ ){
+                     arr[index][countX] 
+            }
+      })
+  }
+
 
   return ( 
    <div className="calendar">
-      <Mes mes={mes}/>
+      {/* <Mes mes={mes}/> */}
       <table className="table-calender">
         <thead>
           <tr>
@@ -26,9 +37,6 @@ export default function Calendar(props) {
         <tbody>
           {/* Criar um loop no dia da semana aqui */}
         
-          <tr>
-            {rows}
-          </tr>
           
         </tbody>
       </table>
--- calendar/src/hooks/useCalendar.ts
+++ calendar/src/hooks/useCalendar.ts
@@ -1,23 +1,28 @@
 import { useEffect, useState } from 'react'
 
-export default function useCalendar() {
+export default function useCalendar(month) {
 
     const [currentmonth, setCurrenMonth] = useState({
         fisrtDayWeek: 0,
         mes: 0,
-        arrDays: []
+        arrDays: new Array()
     })
-  useEffect(() => {  
 
-            let daysMonth = new Date(new Date().getFullYear(), 1, 0);
-            let i = 1
-
-            for (i = 1; i <= daysMonth.getDate(); i++) {
-
-               currentmonth.arrDays.push(i)               
+    const getMonth = () => {
+        let daysMonth = new Date(new Date().getFullYear(), month, 0);       
+        let arrayCurrent = new Array()
+        for(let i = 1;i <= daysMonth.getDate(); i++){
+            arrayCurrent.push(i)
+            if(i % 7 == 0 && i != 1){
+                currentmonth.arrDays.push(arrayCurrent)
+                arrayCurrent = new Array()
             }
-            currentmonth.fisrtDayWeek = daysMonth.getDay()
-        })
-
-    return currentmonth
+        }
+        
+        currentmonth.fisrtDayWeek = daysMonth.getDay();
+    }
+    return {
+             currentmonth,
+             getMonth
+            }
 }
