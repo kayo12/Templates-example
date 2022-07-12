32b0c7c48e69ea410f318514031660a0ea71a139
Kayo campos <kayonbahenrique@gmail.com>
2022-07-12 01:35:34 -0300

    Update logica inserida

--- calendar/src/components/Calendar.tsx
+++ calendar/src/components/Calendar.tsx
@@ -1,34 +1,50 @@
-import Mes  from './Mes'
-import { useState, useEffect, Children } from 'react';
-import useCalendar from '../hooks/useCalendar'
-import Moment from 'moment'
+import Mes from "./Mes";
+import { useState, useEffect, Children } from "react";
+import useCalendar from "../hooks/useCalendar";
+import Moment, { weekdays } from "moment";
 
 export default function Calendar(props) {
+  const [month, setMonth] = useState([]);
 
-    const date = new Date( new Date().getFullYear(),1,0 );
+  const date = Moment([2022, 0]);
+  // Pegando a data do ano
+  // console.log(date.startOf("month").startOf("week").format("D"));
+  // console.log(date.endOf("month").endOf("week").format("D"));
 
-    console.log(Moment().format())
+  useEffect(() => {
 
-  function Week(month: number){
-      let list = new Array()
-      let i = 0;
-      while(i <= date.getDate()){
-          list.push(new Array())
+    let firsDay = date.clone().startOf("month").startOf("week");
+    let endDay = date.clone().endOf("month").endOf("week");
 
-      }
-      useEffect(() =>{
-          console.table(list)
-      })
-    
-     return (
-          <h3></h3>   
-     )
+    console.log(`Primeiro dia do mes ${firsDay}`);
+    console.log(`ultimo dia do mes ${endDay}`);
+
+    while (firsDay.isBefore(endDay.endOf("month"))) {
+      month.push(
+        Array(7)
+          .fill(0)
+          .map(() => firsDay.add(1, "day").clone())
+      );
+    }
+
+  });
+
+  function DaysWeek(props) {
+    return (
+          <span >
+            {props.map((el) => el.value)}
+          </span>
+    )
   }
 
-  return ( 
-   <div className="calendar">
-    <Mes mes={date.getMonth()}/>
-        {/* {Week(1)} */}
+  return (
+    <div className="calendar">
+      <Mes mes={date.month()} />
+      {month.map((current) => (
+       <div key={current}>
+            <DaysWeek key={current} value={current}/>
+        </div> 
+        ))}
     </div>
   );
 }
--- calendar/src/components/Mes.tsx
+++ calendar/src/components/Mes.tsx
@@ -1,15 +1,26 @@
-interface MesAno{
-    mes:  number | string
-    ano?: number
-    horario?: number
+interface MesAno {
+  mes: number | string;
+  ano?: number;
+  horario?: number;
 }
-export default function Mes(props: MesAno){
-
-    let month= ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"] 
-    return(
-        <span className="currentMonth">
-                {month[props.mes]}  
-        </span>
-    )
-
+export default function Mes(props: MesAno) {
+  let month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  return (
+    <div className="blockMonth">
+      <span className="currentMonth">{month[props.mes]}</span>
+    </div>
+  );
 }
