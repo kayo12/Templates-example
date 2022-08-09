78658a0d674c60c2a4189a696c39306fcb08cf69
Kayo campos <kayonbahenrique@gmail.com>
2022-08-09 02:07:33 -0300

    incluindo atualização do calendario com base no ano

--- calendar/src/components/Calendar.tsx
+++ calendar/src/components/Calendar.tsx
@@ -1,24 +1,29 @@
 import Mes from "./Mes";
-import { useEffect, useState, useSyncExternalStore} from "react";
+import { useEffect, useState, useSyncExternalStore } from "react";
 
 export default function Calendar(props) {
-  const [year, setYear] = useState('')
- 
-  function OnChangeYear(){
-      let ye = (document.getElementById("year") as HTMLInputElement).value;
-        console.log(ye)
-       setYear(ye)
+  const [year, setYear] = useState("2021");
+
+  useEffect(() => {
+    OnChangeYear();
+  }, [year]);
+
+  function OnChangeYear() {
+    let ye = (document.getElementById("year") as HTMLInputElement).value;
+    console.log(ye);
+    return setYear(ye);
   }
 
   return (
-      <div className="calendar">
-        <div className="inputYear">
-              <label htmlFor="year">ano</label>
-              <input type="text" id="year" className="year"  />
-            <button onClick={() => {OnChangeYear}}>Buscar</button>
-        </div>
-        <h1>{year}</h1>
-        <Mes ano={Number(year)}/>
+    <div className="calendar">
+      <div className="displayyear">
+        <label htmlFor="year">Ano: {year}</label>
       </div>
+      <div className="inputYear">
+        <input type="text" id="year" className="year" />
+        <button onClick={OnChangeYear}>Buscar</button>
+      </div>
+      <Mes ano={Number(year)} />
+    </div>
   );
 }
--- calendar/src/components/Days.tsx
+++ calendar/src/components/Days.tsx
@@ -1,45 +1,48 @@
-import {useState, useEffect} from "react";                                                                                                                                                     import Moment from "moment";
+import Moment from "moment";
+import { useState, useEffect } from "react";
 
-                                                                                                                                                            export default function Days(props){
-    const [month, setMonth] = useState([]);
-    const [date , setDate] = useState(
-      Moment().locale("pt").month(props.m).year(props.y)
-    )
+export default function Days(props) {
+  const [month, setMonth] = useState([]);
+  const [date, setDate] = useState(
+    Moment().locale("pt").month(props.m).year(props.y)
+  );
 
-    useEffect(() => {
-      let firstDay = date.clone().startOf("month").startOf("week");
-      let endDay = date.clone().endOf("month").endOf("week");
-      let Day = firstDay.clone().subtract(1, "day");
-      console.log(`Primeiro dia do mes ${firstDay}`);
-      console.log(`ultimo dia do mes ${endDay}`);
+  
 
-      let arrayUpdate = []
+  useEffect(() => {
+    
+    let firstDay = date.clone().startOf("month").startOf("week");
+    let endDay = date.clone().endOf("month").endOf("week");
+    let Day = firstDay.clone().subtract(1, "day");
+    console.log(`Primeiro dia do mes ${firstDay}`);
+    console.log(`ultimo dia do mes ${endDay}`);
 
-      while (Day.isBefore(endDay, "day")) {
-        arrayUpdate.push(
-          Array(7)
+    let arrayUpdate = [];
+
+    while (Day.isBefore(endDay, "day")) {
+      arrayUpdate.push(
+        Array(7)
           .fill(0)
-            .map(() => Day.add(1, "day").clone())
-        );
-      }
+          .map(() => Day.add(1, "day").clone())
+      );
+    }
 
-      setMonth(arrayUpdate)  
+    setMonth(arrayUpdate);
+    setDate(date.year(props.y))
+    console.log(month);
+  }, [date,props.y]);
 
-      console.log(month);
-    }, [date]);
-
- return(
+  return (
     <div>
-        {month.map((week) =>
-            <div key={week} className="week">
-               {week.map((d) =>      
-                    <span className="day" key={d._d.getTime() + props.month}>
-                        {d.format("DD").toString()}
-                    </span>
-               )}
-            </div>
-        )}
+      {month.map((week) => (
+        <div key={week} className="week">
+          {week.map((d) => (
+            <span className="day" key={d._d.getTime() + props.month}>
+              {d.format("DD").toString()}
+            </span>
+          ))}
+        </div>
+      ))}
     </div>
- )   
-
+  );
 }
